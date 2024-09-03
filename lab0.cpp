c49ff50d9d8f53f31f1462b8fed8f1d8fa933dc0
Yuliy <Sky.spawta@gmail.com>
2024-09-03 11:23:15 +0300

    update lab0
    Вывод hello world и ввод символа с клавиатуры

--- lab0.cpp
+++ lab0.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
-int main{
-    
+using namespace std;
+
+int main()
+{
+    string sb;
+    cout<<"Hello World!\n";
+    cout << "Input symbol ";
+    cin >> sb;
+    cout<<"Your symbol: "<<sb;
+    return 0;
 }
